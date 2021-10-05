5772f6189f7face1e391fea6e469e5e5b88f33d8
takeru-a <pypy20191022@gmail.com>
2021-10-05 14:41:03 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team18/kaizi/team18/Team18Application.java
@@ -0,0 +1,13 @@
+package oit.is.team18.kaizi.team18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team18Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team18/kaizi/team18/Team18ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team18.kaizi.team18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
